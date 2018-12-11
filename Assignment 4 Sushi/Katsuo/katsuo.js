c36c3794db85208e29d79537e654e69b029694b8
Justin <dbswpghd123@gmail.com>
2018-12-11 07:39:56 -0500

    s

---
+++ Assignment 4 Sushi/Katsuo/katsuo.js
@@ -0,0 +1,240 @@
+$(document).scroll(function(){
+    	console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 0) {
+        	document.getElementById("bonitoonion10").style.position = "absolute";
+        	document.getElementById("bonitoonion10").style.top = "335px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion10").style.position = "fixed";
+        		document.getElementById("bonitoonion10").style.top = "55px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion10").style.position = "fixed";
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 50) {
+        	document.getElementById("bonitoonion9").style.position = "absolute";	
+        	document.getElementById("bonitoonion9").style.top = "385px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion9").style.position = "fixed";
+        		document.getElementById("bonitoonion9").style.top = "105px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion9").style.position = "fixed";
+        	document.getElementById("bonitoonion9").style.top = "335px";
+        	
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 105) {
+        	document.getElementById("bonitoonion8").style.position = "absolute";	
+        	document.getElementById("bonitoonion8").style.top = "435px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion8").style.position = "fixed";
+        		document.getElementById("bonitoonion8").style.top = "155px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion8").style.position = "fixed";
+        	document.getElementById("bonitoonion8").style.top = "330px";
+        	
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 120) {
+        	document.getElementById("bonitoonion7").style.position = "absolute";	
+        	document.getElementById("bonitoonion7").style.top = "455px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion7").style.position = "fixed";
+        		document.getElementById("bonitoonion7").style.top = "175px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion7").style.position = "fixed";
+        	document.getElementById("bonitoonion7").style.top = "335px";
+        	
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 150) {
+        	document.getElementById("bonitoonion6").style.position = "absolute";	
+        	document.getElementById("bonitoonion6").style.top = "495px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion6").style.position = "fixed";
+        		document.getElementById("bonitoonion6").style.top = "215px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion6").style.position = "fixed";
+        	document.getElementById("bonitoonion6").style.top = "345px";
+        	
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 195) {
+        	document.getElementById("bonitoonion5").style.position = "absolute";	
+        	document.getElementById("bonitoonion5").style.top = "545px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion5").style.position = "fixed";
+        		document.getElementById("bonitoonion5").style.top = "265px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion5").style.position = "fixed";
+        	document.getElementById("bonitoonion5").style.top = "350px";
+        	
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 225) {
+        	document.getElementById("bonitoonion4").style.position = "absolute";	
+        	document.getElementById("bonitoonion4").style.top = "575px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion4").style.position = "fixed";
+        		document.getElementById("bonitoonion4").style.top = "295px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion4").style.position = "fixed";
+        	document.getElementById("bonitoonion4").style.top = "350px";
+        	
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 240) {
+        	document.getElementById("bonitoonion3").style.position = "absolute";	
+        	document.getElementById("bonitoonion3").style.top = "585px";
+        	if ($(window).scrollTop() > 278) {
+        		document.getElementById("bonitoonion3").style.position = "fixed";
+        		document.getElementById("bonitoonion3").style.top = "305px";
+        	}
+        }
+
+        else {
+        	document.getElementById("bonitoonion3").style.position = "fixed";
+        	document.getElementById("bonitoonion3").style.top = "345px";
+        	
+        }
+    });
+;
+
+
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 278-74) {
+        	document.getElementById("bonitoonion1").style.position = "fixed";
+        	document.getElementById("bonitoonion1").style.top = 90+74 + $(window).scrollTop()  + "px";
+        	if ($(window).scrollTop() > 288-74) {
+        	document.getElementById("bonitoonion1").style.position = "fixed";
+        	document.getElementById("bonitoonion1").style.top = 378  + "px";}
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 278-74) {
+        	document.getElementById("bonitoginger").style.position = "fixed";
+        	document.getElementById("bonitoginger").style.top = 80 +74 + $(window).scrollTop()  + "px";
+        	if ($(window).scrollTop() > 340-74) {
+        	document.getElementById("bonitoginger").style.position = "fixed";
+        	document.getElementById("bonitoginger").style.top = 420 + "px";}
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 278-148) {
+        	document.getElementById("bonito").style.position = "fixed";
+        	document.getElementById("bonito").style.top = 60+148 + $(window).scrollTop()  + "px";
+        	if ($(window).scrollTop() > 420-148) {
+        	document.getElementById("bonito").style.position = "fixed";
+        	document.getElementById("bonito").style.top = 480  + "px";}
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 278-278) {
+        	document.getElementById("nigiririce").style.position = "fixed";
+        	document.getElementById("nigiririce").style.top = 105+278 + $(window).scrollTop()  + "px";
+        	if ($(window).scrollTop() > 550-278) {
+        	document.getElementById("nigiririce").style.position = "fixed";
+        	document.getElementById("nigiririce").style.top = 655  + "px";}
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 550-278) {
+            document.getElementById("bonito").style.left = 550 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("nigiririce").style.left = 610 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoginger").style.left = 690 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion1").style.left = 747 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion2").style.left = 710 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion3").style.left = 735 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion4").style.left = 720 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion5").style.left = 740 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion6").style.left = 720 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion7").style.left = 740 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion8").style.left = 713 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion9").style.left = 708 + 278 - $(window).scrollTop()  + "px";
+            document.getElementById("bonitoonion10").style.left = 730 + 278 - $(window).scrollTop()  + "px";
+        }
+    });
+;
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 529) {
+            document.getElementById("katsuoinfo").style.opacity = 1;
+        }
+        else {
+            document.getElementById("katsuoinfo").style.opacity = 0;
+        }
+    });
+;            
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 529) {
+            document.getElementById("gingerinfo").style.opacity = 1;
+        }
+        else {
+            document.getElementById("gingerinfo").style.opacity = 0;
+        }
+    });
+;       
+
+$(document).scroll(function(){
+        if ($(window).scrollTop() > 130) {
+            document.getElementById("katsuoname").style.opacity = 0;
+        }
+        else {
+            document.getElementById("katsuoname").style.opacity = 1;
+        }
+    });
+;       
+
+
+
